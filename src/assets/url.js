fc341158fb8e02c629fb9b14b8827fbfa45f54d4
heimusu <heimusu@gmail.com>
2017-04-10 16:21:33 +0900

    last build

--- src/assets/url.js
+++ src/assets/url.js
@@ -4,4 +4,4 @@
 window.url = '104.198.126.243:3000';
 // window.statusUrl = '52.192.55.36:3000';
 // window.statusUrl = '52.68.45.211:3000';
-windiw.statusUrl = '13.113.185.123:3000';
+window.statusUrl = '13.113.185.123:3000';
